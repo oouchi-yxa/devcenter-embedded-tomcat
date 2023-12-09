799c4cff932c270c4f099432d96be51fb2fc7836
oouchi-yxa <134025597+oouchi-yxa@users.noreply.github.com>
2023-12-09 14:53:19 +0900

    Update HelloServlet.java

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -18,6 +18,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
         ServletOutputStream out = resp.getOutputStream();
         out.write("hello heroku サンプル 日本語を書いてみる".getBytes("UTF-8"));
         out.flush();

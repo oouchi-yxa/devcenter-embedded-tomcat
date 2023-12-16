afb1296f1a2e3401316f588c76d67a2e2f67777c
oouchi-yxa <oouchi-yxa@nec.com>
2023-12-16 17:41:44 +0900

    CloudCube組み込み準備

--- src/main/java/servlet/FileServlet2.java
+++ src/main/java/servlet/FileServlet2.java
@@ -21,8 +21,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Map;
 
 @WebServlet(
@@ -37,6 +41,7 @@ public class FileServlet2 extends HttpServlet {
 
         Map<String, String> env = System.getenv();
 
+        // CloudCube設定の参照
         String cloudcubeAccessKeyId = env.get("CLOUDCUBE_ACCESS_KEY_ID");
         String cloudcubeSecretAccessKey = env.get("CLOUDCUBE_SECRET_ACCESS_KEY");
         String cloudcubeUrl = env.get("CLOUDCUBE_URL");
@@ -48,8 +53,56 @@ public class FileServlet2 extends HttpServlet {
         S3Client s3Client =
                 S3Client.builder()
                         .credentialsProvider(DefaultCredentialsProvider.create())
-                        .region(Region.US_EAST_1)
+                        .region(Region.US_EAST_2)
                         .build();
+
+        try {
+            // https://cloud-cube-us2.s3.amazonaws.com/yygxgacc5b8a
+            ListObjectsRequest listObjects = ListObjectsRequest
+                    .builder()
+                    .bucket("cloud-cube-us2")
+                    .prefix("yygxgacc5b8a/")
+                    .build();
+
+            ListObjectsResponse res = s3Client.listObjects(listObjects);
+            List<S3Object> objects = res.contents();
+            for (S3Object myValue : objects) {
+                System.out.print("\n The name of the key is " + myValue.key());
+                System.out.print("\n The owner is " + myValue.owner());
+            }
+
+            /*
+            System.out.print("\n data get 0");
+
+            GetObjectRequest objectRequest = GetObjectRequest
+                    .builder()
+                    .key("hxlzsmew3hjg/public/tree_sample.html")
+                    .bucket("cloud-cube-us2")
+                    .build();
+
+            System.out.print("\n data get 1");
+
+            ResponseBytes<GetObjectResponse> objectBytes = s3Client.getObjectAsBytes(objectRequest);
+            byte[] data = objectBytes.asByteArray();
+
+            System.out.print("\n data get 2");
+
+            // Write the data to a local file.
+            File myFile = new File("/tmp/test.txt");
+            OutputStream os = new FileOutputStream(myFile);
+            os.write(data);
+            System.out.println("Successfully obtained bytes from an S3 object");
+            os.close();
+
+             */
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+
 
         resp.setContentType("text/html;charset=UTF-8");
 
